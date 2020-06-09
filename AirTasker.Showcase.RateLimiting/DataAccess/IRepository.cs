1f4da670a1a1634c8bb46061d4bba739e8339b0a
SarahNayeriAlinta <sarah.nayeri@alintaenergy.com.au>
2020-06-09 10:49:53 +1000

    Removed unnecessary project

--- AirTasker.Showcase.RateLimiting/DataAccess/IRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AirTasker.Showcase.RateLimiting.DataAccess
-{
-    public interface IRepository
-    {
-        double CalculateWaitingTime(string userId, int rateLimit, double IntervalInSeconds);
-    }
-}
